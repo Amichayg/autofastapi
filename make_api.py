df82b125a0aba2b24e35cc49b098e4349450d23d
amichaygiuili <amichayg@icloud.com>
2022-11-30 00:53:18 +0200

    added add to app function to aut0 add func to fastapi

--- make_api.py
+++ make_api.py
@@ -3,6 +3,7 @@ from pydantic import create_model
 from inspect import signature, Signature, Parameter
 import inspect
 from typing import Callable
+from arity import is_zero
 
 
 def signature_to_model(sig: Signature):
@@ -21,14 +22,33 @@ def func_as_pydantic_model(func: Callable):
     return create_model(func.__name__, **signature_to_model(signature(func)))
 
 
+def hello():
+    return "whatsup"
+
+
 def f(a: int):
+    """ increments a by 1"""
     return a + 1
 
 
 app = FastAPI()
-app.get("/")(lambda: "hello fastapi")
 
 
-@app.post("/f")
-async def do_f(args: func_as_pydantic_model(f)):
-    return f(**args.dict())
+def add_to_app(func: Callable):
+    global app
+    if is_zero(func):
+        app.get(f"/{func.__name__}")(func)
+    else:
+        model = func_as_pydantic_model(func)
+
+        def inner(args: model):
+            """panda"""
+            return func(**args.dict())
+
+        inner.__name__ = func.__name__
+        inner.__doc__ = func.__doc__
+        app.post(f'/{func.__name__}')(inner)
+
+
+add_to_app(f)
+add_to_app(hello)
